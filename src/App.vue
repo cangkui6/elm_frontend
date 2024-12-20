5fe850a1a2847944e0703ce3d36d9bb8b7534e13
cangkui6 <2254600749@qq.com>
2024-12-21 00:01:21 +0800

    提交路由以及main.js的axios配置

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,42 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="app">
+    <router-view/>
+  </div>
 </template>
-
+<!-- 这里是共通样式，适用于所有组件，所以不要加scoped -->
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+	html,
+	body,
+	div,
+	span,
+	h1,
+	h2,
+	h3,
+	h4,
+	h5,
+	h6,
+	ul,
+	ol,
+	li,
+	p {
+		margin: 0;
+		padding: 0;
+	}
 
-nav {
-  padding: 30px;
-}
+	html,
+	body,
+	#app {
+		width: 100%;
+		height: 100%;
+		font-family: "微软雅黑";
+	}
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+	ul,
+	ol {
+		list-style: none;
+	}
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+	a {
+		text-decoration: none;
+	}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,69 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue'; // Vue 3 的入口方法
+import App from './App.vue';
+// 路由
+import router from './router';
+// 字体库
+import 'font-awesome/css/font-awesome.min.css';
+// ajax封装好的库，专门用来发送异步请求
+import axios from 'axios';
+// qs 是一个用于序列化和反序列化查询字符串的库
+import qs from 'qs';
+// 通用的工具类，'./common.js'，多个方法，导入了对象
+import {
+	getCurDate,
+	setSessionStorage,
+	getSessionStorage,
+	removeSessionStorage,
+	setLocalStorage,
+	getLocalStorage,
+	removeLocalStorage,
+} from './common.js';
 
-createApp(App).use(router).mount('#app')
+// 创建 Vue 3 应用实例
+const app = createApp(App);
+
+// 配置 axios 的基础 URL
+axios.defaults.baseURL = 'http://localhost:8080/elm/';
+
+// 将 axios 和 qs 挂载到全局实例上
+app.config.globalProperties.$axios = axios;
+app.config.globalProperties.$qs = qs;
+
+// 将常用的工具方法挂载到全局实例上
+app.config.globalProperties.$getCurDate = getCurDate;
+app.config.globalProperties.$setSessionStorage = setSessionStorage;
+app.config.globalProperties.$getSessionStorage = getSessionStorage;
+app.config.globalProperties.$removeSessionStorage = removeSessionStorage;
+app.config.globalProperties.$setLocalStorage = setLocalStorage;
+app.config.globalProperties.$getLocalStorage = getLocalStorage;
+app.config.globalProperties.$removeLocalStorage = removeLocalStorage;
+
+// 配置全局路由守卫
+router.beforeEach((to, from, next) => {
+	const user = sessionStorage.getItem('user');
+	// 除了登录、注册、首页、商家列表、商家信息之外，都需要判断是否登录
+	if (
+		!(
+			to.path === '/' ||
+			to.path === '/index' ||
+			to.path === '/businessList' ||
+			to.path === '/businessInfo' ||
+			to.path === '/login' ||
+			to.path === '/register'
+		)
+	) {
+		if (user == null) {
+			// 跳转到登录页面
+			router.push('/login').catch((err) => {
+				if (err.name !== 'NavigationDuplicated') {
+					console.error(err);
+				}
+			});
+			location.reload();
+		}
+	}
+	next();
+});
+
+// 挂载路由和应用实例
+app.use(router).mount('#app');
